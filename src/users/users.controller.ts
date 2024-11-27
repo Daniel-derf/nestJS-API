ca0b054a547c3c189f0c9393c78ff36c2ff690a7
Daniel-derf <danielpardinho321@gmail.com>
2024-11-27 13:34:19 -0300

    basic CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,45 +1,41 @@
 import { Controller, Get, Post, Put, Delete, Param, Query, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
 
   // GET /users -> []
   @Get()
-  getUsers(@Query('type') type: string) {
-    return [{type}];
+  getUsers(@Query('type') type: 'developer' | 'pedreiro') {
+
+    return this.usersService.getUsers(type)
   }
 
   // GET /users/:id -> {...}
   @Get(':id')
   getOneUser(@Param('id') id: string) {
-    return {
-      id
-    };
+    return this.usersService.getOneUser(+id)
   }
 
   // POST /users
   @Post()
   createUser(@Body() createUserDto: CreateUserDto){
-    return {
-      'name': createUserDto.name
-    };
+    return this.usersService.createUser(createUserDto)
   }
 
   // PUT /users/:id -> {...}
   @Put(':id')
-  updateUser(@Param('id') id: string) {
-    return {
-      id
-    };
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUser(+id, updateUserDto)
   }
 
   // DELETE /users/:id
   @Delete(':id')
   removeUser(@Param('id') id: string) {
-    return {
-      id
-    };
+    return this.usersService.removeUser(+id)
   }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'Daniel', type: 'developer' },
+    { id: 2, name: 'Junior', type: 'pedreiro' },
+  ]
+
+  getUsers(type?: 'developer' | 'pedreiro') {
+    if (type) {
+      return this.users.filter(user => user.type === type)
+    }
+
+    return this.users
+  }
+
+  getOneUser(id: number){
+    const user = this.users.find((user) => user.id === id)
+
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    return user
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = {
+      ...createUserDto,
+      id: Date.now()
+    }
+    this.users.push(newUser)
+  }
+
+  updateUser(id:number, updateUserDto: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUserDto }
+      }
+
+      return user;
+    });
+
+    return this.getOneUser(id);
+  }
+
+  removeUser(id: number){
+    const toBeRemoved = this.getOneUser(id);
+
+    this.users = this.users.filter(user => user.id !== id);
+
+    return toBeRemoved;
+  }
+
+
+}
+
